765e387547259a0c0c77476a008c73b35c7e9b4f
Yasar icli <yasaricli@gmail.com>
2019-11-05 02:15:04 +0300

    lit fixed

--- packages/tatsy-collection/utils/collection.js
+++ packages/tatsy-collection/utils/collection.js
@@ -12,65 +12,65 @@ const { apiUrl } = require('./shortcuts');
 module.exports = (name, options) => {
   const { schema = {} } = options;
 
-    return (http, url) => {
-      const Model = mongoose.model(name, schema);
+  return (http, url) => {
+    const Model = mongoose.model(name, schema);
 
-      // getAll
-      http.get(apiUrl(url), (req, res) => {
-        Model.find({ }, (err, list) => {
-          res.json(returnJSON('success', list));
-        });
+    // getAll
+    http.get(apiUrl(url), (req, res) => {
+      Model.find({ }, (err, list) => {
+        res.json(returnJSON('success', list));
       });
+    });
 
-      http.get(apiUrl(url, '_id'), (req, res) => {
-        const { _id } = req.params;
+    http.get(apiUrl(url, '_id'), (req, res) => {
+      const { _id } = req.params;
 
-        return Model.findOne({ _id }, (err, doc) => {
-          if (doc) {
-            return res.json(returnSuccess(doc));
-          }
+      return Model.findOne({ _id }, (err, doc) => {
+        if (doc) {
+          return res.json(returnSuccess(doc));
+        }
 
-          return res.json(returnFail(name));
-        });
+        return res.json(returnFail(name));
       });
+    });
 
-      http.post(apiUrl(url), (req, res) => {
-        const { body } = req;
+    http.post(apiUrl(url), (req, res) => {
+      const { body } = req;
 
-        // create model
-        const doc = new Model(body);
+      // create model
+      const doc = new Model(body);
 
-        // save doc
-        return doc.save((err, data) => {
+      // save doc
+      return doc.save((err, data) => {
+        return res.json(returnSuccess(data));
+      });
+    });
+
+    http.put(apiUrl(url, '_id'), (req, res) => {
+      const { _id } = req.params;
+      const { body } = req;
+
+      // update
+      Model.findOneAndUpdate({ _id }, body, (err, data) => {
+        if (data) {
           return res.json(returnSuccess(data));
-        });
+        }
+
+        return res.json(returnFail(_id));
       });
+    });
 
-      http.put(apiUrl(url, '_id'), (req, res) => {
-        const { _id } = req.params;
-        const { body } = req;
+    http.delete(apiUrl(url, '_id'), (req, res) => {
+      const { _id } = req.params;
 
-        // update
-        Model.findOneAndUpdate({ _id }, body, (err, data) => {
-          if (data) {
-            return res.json(returnSuccess(data));
-          }
+      // remove
+      return Model.findOneAndDelete({ _id }, (err, data) => {
+        if (data) {
+          return res.json(returnSuccess(data));
+        }
 
-          return res.json(returnFail(_id));
-        });
+        return res.json(returnFail(_id));
       });
-
-      http.delete(apiUrl(url, '_id'), (req, res) => {
-        const { _id } = req.params;
-
-        // remove
-        return Model.findOneAndDelete({ _id }, (err, data) => {
-          if (data) {
-            return res.json(returnSuccess(data));
-          }
-
-          return res.json(returnFail(_id));
-        });
-      });
-    };
+    });
+  };
 };
