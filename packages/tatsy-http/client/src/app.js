26ee80fa5527b26eb113a324704b5e4160709aa3
Yasar icli <yasaricli@gmail.com>
2019-11-07 02:23:23 +0300

    tatsy-http: Docs UI #12

--- packages/tatsy-http/client/src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// STYLE
-import '../scss/main.scss';
-
-const template = <p>Hello from react</p>;
-
-ReactDOM.render(template, document.getElementById('root'));
